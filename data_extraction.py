bb8664b316d58d15757a8cb89a0d137ab0646020
Gangxin-Li <lgx.uofg@gmail.com>
2023-01-28 00:42:48 +0000

    M2T3

--- data_extraction.py
+++ data_extraction.py
@@ -1,4 +1,6 @@
 from database_utils import DatabaseConnector
 class DataExtractor:
-    def list_db_tables():
+    def list_db_tables(slef,):
         database =DatabaseConnector.init_db_engine()
+    def read_rds_table(self,):
+
--- database_utils.py
+++ database_utils.py
@@ -1,9 +1,23 @@
 import yaml
+import psycopg2
+
 class DatabaseConnector:
+    def __init__(self):
+        pass
     def read_db_creds(self):
         with open('db_creds.yaml') as f:
             data = yaml.safe_load(f)
         return data
     def init_db_engine(self):
         credentials = self.read_db_creds()
-        return credentials
+        with psycopg2.connect(host=credentials['RDS_HOST'], user=credentials['RDS_USER'], password=credentials['RDS_PASSWORD'], dbname=credentials['RDS_DATABASE'], port=credentials['RDS_PORT']) as conn:
+            with conn.cursor() as cur:
+                records = cur.fetchall()
+        print(records)
+
+        return records
+
+
+
+ins = DatabaseConnector()
+ins.init_db_engine()
